57e3f1ca0d23251960086946ab5a2cc52a2f6418
ResilientProgramer <52796008+ResilientProgramer@users.noreply.github.com>
2019-08-10 02:09:08 -0400

    Add files via upload

---
+++ assets/Data/mytravelinfo.ts
@@ -0,0 +1,6 @@
+import {TRAVELINFO} from '../../app/travelinfo';
+
+export const MYTRAVELINFO : TRAVELINFO[] = [
+        {commutetype:'Car' , time:'30 minute commute by car?',goby:'Car' },
+        {commutetype: "Bus",  time: "Bus and Train-1 hour commute",goby:'Bus'}
+]
---
+++ assets/Data/schedule.ts
@@ -0,0 +1,6 @@
+import {BUSSCHEDULE } from '../../app/BusSchedule';
+
+export const SCHEDULE : BUSSCHEDULE[] =[
+    {route:'South Trafalgar   ', leavetime:'8:00 am',arrivetime:'10:30 am'},
+    {route:'East MiddleUpper   ', leavetime:'9:15 am', arrivetime: '11:45 am'}
+]
